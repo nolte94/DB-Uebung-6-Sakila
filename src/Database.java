dd9f1d19f7e47147cebde8bdcabd9f61ce0430c4
286976 <286976@TI-PC64.fh-luebeck.de>
2017-01-12 11:24:46 +0100

    Add connection sucessfully

--- src/Database.java
+++ src/Database.java
@@ -1,8 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Database {
+	
+	private static String user = "Mo14a_1", pass = "dDGpq8DXGe4Q",host = "slo.swe.fh-luebeck.de",db=user;
 
 	public static void main(String[] args) {
-		//Test
+		try {
+			// DBS‐JDBC‐Treiber laden
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// DBS‐Verbindung herstellen
+			Connection conn =
+			DriverManager.getConnection("jdbc:mysql://"+host+"/"+db+"?user="+user+"&password="+pass);
+//			// Statement erzeugen and Query ausführen
+//			Statement stmt = conn.createStatement();
+//			ResultSet res = stmt.executeQuery("SELECT isbn, title FROM book");
+//			Integer columns = res.getMetaData().getColumnCount();
+//			// ResultSet verarbelten
+//			while (res.next()) {
+//			for (int i = 1; i <= columns; i++) {
+//			Object o = res.getObject(i);
+//			System.out.println(o);
+//			}
+			conn.close();
+			//}
+			//res.close();
+			//stmt.close();
+			} catch (SQLException | ClassNotFoundException ex) {
+				ex.printStackTrace();
+			}
 	}
 
 }
